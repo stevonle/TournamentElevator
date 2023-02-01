e0cf7c4c93d499303b9b01a9bbbbca0249ba5edb
Alec Mason <masonalecjordan@gmail.com>
2023-02-01 10:42:58 -0600

    Add navbar and redo team create styling

--- capstone/vue/src/App.vue
+++ capstone/vue/src/App.vue
@@ -1,9 +1,22 @@
 <template>
   <div id="app">
     <div id="nav" class="navbar navbar-light bg-light">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      <!-- <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link> -->
+      <nav-bar/>
     </div>
     <router-view />
   </div>
 </template>
+
+<script>
+
+import NavBar from '/src/components/NavBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    'nav-bar' : NavBar
+  }
+};
+</script>
---
+++ capstone/vue/src/components/NavBar.vue
@@ -0,0 +1,41 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-light bg-light" v-bind:class=" { 'navbarOpen': show }">
+        <a class="navbar-brand" href="#">Tournament Elevator</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" @click.stop="toggleNavbar()">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent" v-bind:class="{ 'show': show }">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/teams/create">Create Team</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      Tournaments
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">View Tournaments</a>
+                        <a class="dropdown-item" href="/tournament/create">Create Tournaments</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">My Tournaments</a>
+                    </div>
+                    </li>
+            </ul>
+            <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+             <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "NavBar"
+}
+</script>
+
+<style>
+
+</style>
