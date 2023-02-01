c471b91ba4c1b56b7d1b8a58c2127440b327c5fa
Alec Mason <masonalecjordan@gmail.com>
2023-02-01 13:09:35 -0600

    implement teamcard/teamview/router paths for browse teams

---
+++ capstone/vue/src/views/Teams.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <teams-list />
+  </div>
+</template>
+
+<script>
+import TeamsList from '../components/TeamsList.vue'
+export default {
+    name: "Teams",
+    component: {TeamsList}
+}
+</script>
+
+<style scoped>
+</style>
