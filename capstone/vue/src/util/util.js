e025c5c18bd4251c6eaf955b72977214c29fae21
chase <chasemflowers@gmail.com>
2023-02-09 10:34:27 -0500

    Adding reactive icons to tournament card based on gametype

--- capstone/vue/src/util/util.js
+++ capstone/vue/src/util/util.js
@@ -15,6 +15,21 @@ export function getGameName(type) {
   }
 }
 
+export function getGameIcon(gameId) {
+  switch (gameId) {
+    case 1:
+      return "img/Icons/football.png";
+    case 2:
+      return "img/Icons/soccer.png";
+    case 3:
+      return "img/Icons/basketball.png";
+    case 4:
+      return "img/Icons/volleyball.png";
+    default:
+      return "img/Icons/default.png";
+  }
+}
+
 export function isHost(tournament) {
   if (!tournament) return false;
   return this.$store.state.user.id === tournament.host;
